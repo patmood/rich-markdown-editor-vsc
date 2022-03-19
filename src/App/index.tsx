24d7b8a8e5e11020de2099f28170e3f7cea23a4e
Patrick <4031292+patmood@users.noreply.github.com>
2022-03-18 22:09:21 -0700

    theme the editor

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,9 +1,12 @@
 import React, { useEffect, useState } from "react"
 
 import Editor from "rich-markdown-editor"
+import { vsCodeTheme } from "./theme"
+
+const PLACEHOLDER = "Enjoy that new doc smell..."
 
 export function App({ vscode }: { vscode: any }) {
-  const { text: defaultValue } = vscode.getState()
+  const defaultValue = vscode.getState()?.text
   const [value, setValue] = useState(defaultValue)
 
   function handleChange(getVal: () => string) {
@@ -18,14 +21,14 @@ export function App({ vscode }: { vscode: any }) {
   useEffect(() => {
     function messageHandler(event: MessageEvent) {
       const message = event.data // The json data that the extension sent
-      const { text: currentText } = vscode.getState()
-      const { type, text: newText } = message
+      const currentText = vscode.getState()?.text
+      const { type, text } = message
       switch (type) {
         case "update":
           // If the update came from another window, trigger editor rerender
-          if (newText !== currentText) {
-            setValue(message.text)
-            vscode.setState({ text: newText })
+          if (text !== currentText) {
+            setValue(text)
+            vscode.setState({ text })
           }
           return
       }
@@ -38,10 +41,13 @@ export function App({ vscode }: { vscode: any }) {
   return (
     <div style={{ padding: 40, maxWidth: 825, margin: "0 auto" }}>
       <Editor
+        placeholder={PLACEHOLDER}
+        theme={vsCodeTheme}
         defaultValue={defaultValue}
         value={value}
         onChange={handleChange}
         onSave={() => console.log("save!")}
+        autoFocus
       />
     </div>
   )
---
+++ src/App/theme.ts
@@ -0,0 +1,33 @@
+import { theme } from "rich-markdown-editor"
+
+// Editor variables: https://github.com/outline/rich-markdown-editor/blob/main/src/styles/theme.ts
+// VSCode variables: https://code.visualstudio.com/api/references/theme-color
+// CSS variable naming: "var(--vscode-XXX)"
+export const vsCodeTheme = {
+  ...theme,
+  link: "var(--vscode-textLink-foreground)",
+
+  background: "var(--vscode-editor-background)",
+  text: "var(--vscode-editor-foreground)",
+  code: "var(--vscode-textPreformat-foreground)",
+  cursor: "var(--vscode-editorCursor-foreground)",
+  divider: "var(--vscode-editorRuler-foreground)",
+
+  toolbarBackground: "var(--vscode-button-background)",
+  toolbarHoverBackground: "var(--vscode-button-hoverBackground)",
+  toolbarInput: "var(--vscode-button-secondaryForeground)",
+  toolbarItem: "var(--vscode-button-foreground)",
+
+  tableDivider: "var(--vscode-editorRuler-foreground)",
+  tableSelected: "var(--vscode-editorRuler-foreground)",
+  tableSelectedBackground: "var(--vscode-editor-wordHighlightBackground)",
+
+  quote: "var(--vscode-textBlockQuote-background)",
+  codeBackground: "var(--vscode-textCodeBlock-background)",
+  codeBorder: "transparent",
+  horizontalRule: "var(--vscode-editorRuler-foreground)",
+  imageErrorBackground: "var(--vscode-textBlockQuote-background)",
+
+  scrollbarBackground: "var(--vscode-scrollbarSlider-background)",
+  // scrollbarThumb: colors.greyMid,
+}
